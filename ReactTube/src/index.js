c1d960ba28ef57905e7801dc7d77d51830b7669f
David McCormick <dave.a.mccormick@gmail.com>
2018-06-17 09:39:50 +1000

    Display simple input

--- ReactTube/src/index.js
+++ ReactTube/src/index.js
@@ -1,11 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { Globals } from './helpers/globals'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Globals } from './helpers/globals';
+
+import SearchBar from './components/search_bar';
 
 // Create a new component that produces html
 const App = () => {
-    return <div>Hi!</div>;
-}
+    return (
+        <div>
+            <SearchBar />
+        </div>
+    );
+};
 
 // Add to the DOM
 ReactDOM.render(<App />, document.querySelector(".container"));
