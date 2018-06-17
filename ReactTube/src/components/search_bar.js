09b9fc1a040f7f9b5cdf4b4d54800f0e0c51a16e
David McCormick <dave.a.mccormick@gmail.com>
2018-06-17 16:51:44 +1000

    Clickable rows, and better CSS

--- ReactTube/src/components/search_bar.js
+++ ReactTube/src/components/search_bar.js
@@ -9,13 +9,11 @@ class SearchBar extends Component {
 
     render() {
         return (
-            <div>
+            <div className="search-bar">
                 <input
                     value={ this.state.term } 
                     onChange={ (event) => this.onInputChange(event.target.value) } 
                 />
-
-                Value of input: { this.state.term }
              </div>
         );
     }
