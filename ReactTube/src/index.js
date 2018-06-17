a0c5776ac91344a4184b7f9d6cd7e374f126a40b
David McCormick <dave.a.mccormick@gmail.com>
2018-06-17 16:36:38 +1000

    Add basic UI for showing youtube video

--- ReactTube/src/index.js
+++ ReactTube/src/index.js
@@ -1,17 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+
 import { Globals } from './helpers/globals';
-
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
-// Create a new component that produces html
-const App = () => {
-    return (
-        <div>
-            <SearchBar />
-        </div>
-    );
-};
+class App extends Component {
+    constructor(props) {
+        super(props);
 
-// Add to the DOM
+        this.state = { 
+            videos: [],
+            selectedVideo: null
+        };
+
+        YTSearch({ key: Globals.API_KEY, term: "surfboards" }, (videos) => {
+            this.setState({ 
+                videos: videos,
+                selectedVideo: videos[0] 
+            });
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <SearchBar />
+                <VideoDetail video={this.state.selectedVideo} />
+                <VideoList videos={this.state.videos} />
+            </div>
+        );
+    }
+}
+
 ReactDOM.render(<App />, document.querySelector(".container"));
