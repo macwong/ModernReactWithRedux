a88751dff12e983a6adf6cdf448d3d77dc82a9f2
David McCormick <dave.a.mccormick@gmail.com>
2018-06-20 18:00:53 +1000

    Create a weather list on search

--- Weather/src/reducers/reducer_weather.js
+++ Weather/src/reducers/reducer_weather.js
@@ -1,9 +1,10 @@
-import { FETCH_WEATHER } from "../actions";
+import { FETCH_WEATHER } from "../actions/index";
 
 export default function(state = [], action) {
     switch(action.type) {
         case FETCH_WEATHER: {
-            
+            // Ensure we return a new instance of state
+            return state.concat([ action.payload.data ]);
         }
     }
 
