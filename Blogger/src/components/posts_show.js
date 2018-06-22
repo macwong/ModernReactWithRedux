8bbe5b9b2beb48fc610631e342b8587a08643da5
David McCormick <dave.a.mccormick@gmail.com>
2018-06-22 15:14:09 +1000

    Basic route to show post

---
+++ Blogger/src/components/posts_show.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class PostShow extends Component {
+    render() {
+        return (
+            <div>Show Post</div>
+        );
+    }
+}
+
+export default PostShow;
