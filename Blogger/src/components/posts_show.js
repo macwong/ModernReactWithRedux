b38c4750969ede0d2b2418e3fbd293ef1c305983
David McCormick <dave.a.mccormick@gmail.com>
2018-06-22 18:26:05 +1000

    Get post and delete post

--- Blogger/src/components/posts_show.js
+++ Blogger/src/components/posts_show.js
@@ -1,11 +1,47 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { fetchPost, deletePost } from '../actions';
 
 class PostShow extends Component {
+    componentDidMount() {
+        const id = this.props.match.params.id;
+        this.props.fetchPost(id);
+    }
+
+    onDeleteClick() {
+        this.props.deletePost(this.props.match.params.id, () => {
+            this.props.history.push('/');
+        });
+    }
+
     render() {
+        if (!this.props.post) {
+            return <div>Loading...</div>;
+        }
+
+        console.log(this.props.post);
         return (
-            <div>Show Post</div>
+            <div>
+                <Link to="/">Back to Index</Link>
+                <button
+                    className="btn btn-danger pull-xs-right"
+                    onClick={this.onDeleteClick.bind(this)}
+                >
+                    Delete Post
+                </button>
+                <h3>{this.props.post.title}</h3>
+                <div>Categories: {this.props.post.categories}</div>
+                <div>{this.props.post.content}</div>
+            </div>
         );
     }
 }
 
-export default PostShow;
+function mapStateToProps(state, ownProps) {
+    return {
+        post: state.posts[ownProps.match.params.id]
+    }
+}
+
+export default connect(mapStateToProps, { fetchPost, deletePost })(PostShow);
